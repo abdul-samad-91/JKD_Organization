ba48006dcfc7ab38ee92d67ea9e1e32a36827eb2
as03489822 <as03489822@gmail.com>
2025-08-20 11:00:36 +0500

    make hero section responsive

--- src/app/page.js
+++ src/app/page.js
@@ -12,21 +12,17 @@ export default function Home() {
     < div className="h-screen w-full  flex flex-col justify-between">
       <Header />
       <div className="flex flex-col items-center ">
-        {/* <section className="h-[560px] lg:w-[1200px] flex ">
-          <div className="w-[50%] h-full text-start flex flex-col justify-center">
-            <h1 className="text-[39px] font-bold  w-[80%]">Together for a Brighter Future</h1>
-            <p className="text-[22px] w-[80%] mt-4">JKD Pakistan is a social development and welfare organization dedicated to uplifting communities through education, healthcare, youth empowerment, and community development.</p>
-            <button className="text-[19px] bg-[#00874F] w-[40%] mt-2 rounded p-2 px-2 text-white">Be a part of the change!</button>
-          </div>
-          <div className="relative w-[50%] h-[full] ">
-            <Image src={jkdPhoto} alt="JKD PHOTO"  className="object-cover rounded" fill/>
-          </div>
-        </section> */}
-        <section style={{ backgroundImage: `url(${jkdPhoto.src})` }} className={` h-[560px]  bg-cover bg-center ${theme === 'light' ? 'text-black': 'text-white'}`}>
-          <div className={`w-full h-full text-start flex ${theme === 'light' ? '' : 'bg-black/10'}  flex-col  justify-center pl-20 `}>
-            <h1 className="text-[39px] font-bold  " style={{ textShadow: `2px 2px 4px rgba(${theme === 'light'?'255,255,255':'0,0,0'},0.7)` }}>Together for a Brighter Future</h1>
-            <p className="text-[22px] w-[46%] mt-4  " style={{ textShadow: `2px 2px 4px rgba(${theme === 'light'?'255,255,255':'0,0,0'},0.9)` }}>JKD Pakistan is a social development and welfare organization dedicated to uplifting communities through education, healthcare, youth empowerment, and community development.</p>
-            <button className={`text-[19px] ${theme === 'light' ? 'bg-[#00874F]': 'bg-[#177faa]'} text-white  w-[20%] mt-2 rounded p-2 px-2 `}>Be a part of the change!</button>
+        <section 
+          className={`relative h-[400px] md:h-[460px] lg:h-[560px] bg-cover bg-center`}
+        >
+            <div 
+              className="absolute inset-0 bg-cover bg-center blur-[1px]" 
+              style={{ backgroundImage: `url(${jkdPhoto.src})` }} 
+            />
+          <div className={`relative z-10 w-full h-full text-start flex ${theme === 'light' ? ' text-black bg-white/5' : 'text-white bg-black/10'}  flex-col  justify-center px-6 lg:pl-20 `}>
+            <h1 className="text-[30px] md:text-[34px] lg:text-[39px] font-bold sm:w-[70%] md:w-[65%] lg-w-full " style={{ textShadow: `2px 2px 4px rgba(${theme === 'light'?'255,255,255':'0,0,0'},0.7)` }}>Together for a Brighter Future</h1>
+            <p className="text-justify text-[16px] md:text-[17px] lg:text-[22px] sm:w-[60%] md:w-[55%] lg:w-[46%] mt-5  " style={{ textShadow: `2px 2px 4px rgba(${theme === 'light'?'255,255,255':'0,0,0'},0.9)` }}>JKD Pakistan is a social development and welfare organization dedicated to uplifting communities through education, healthcare, youth empowerment, and community development.</p>
+            <button className={`text-sm md:text-[16px] lg:text-[19px] ${theme === 'light' ? 'bg-[#00874F]': 'bg-[#177faa]'} text-white w-[59%] sm:w-[30%] md:w-[25%] lg:w-[20%] mt-2 rounded p-2 sm:px-2 `}>Be a part of the change!</button>
           </div>
         </section>
       </div>
