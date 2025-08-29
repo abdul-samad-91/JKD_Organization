ee61e624d70ad8fe852694cf6aac3643d26086f2
as03489822 <as03489822@gmail.com>
2025-08-30 00:19:50 +0500

    change

--- src/app/programs/page.js
+++ src/app/programs/page.js
@@ -18,7 +18,7 @@ import Image from 'next/image'
 
 const allprograms = [
   {
-    title: "TVETA",
+    title: "IT and Digital Skills",
     description: "As a key component of JKD PAKISTAN, TVETA -Technical and Vocational Education and Training",
     fullDescription : "As a key component of JKD PAKISTAN, TVETA -Technical and Vocational Education and Training Academy is a premier institution provides customized training programs, consultancy services, skills development courses, professional certifications and Capacity building to empower individuals and organizations, fostering growth and development. JKD TVETA also offers flexible and convenient trainings and consultancy services, delivered right to your home, office and organization.",
     pIcon: program1,
@@ -30,49 +30,43 @@ const allprograms = [
     pIcon:program9,
   },
   {
-    title: "Sports-Spark",
+    title: "Sports and Fitness",
     description: "As a flagship initiative and an inclusive Sports Centre of the JKD PAKISTAN, dedicated to igniting",
     fullDescription: "As a flagship initiative and an inclusive Sports Centre of the JKD PAKISTAN, dedicated to igniting a passion for sports in everyone. Our mission is to provide a welcoming and accessible environment where people of all abilities can come together to play, learn, and grow. Fields of play, organizing sports events and trainings in JKD Sports Arena & JKD Sports Academy are available both outdoors and indoors.",
     pIcon:program2 ,
   },
   {
-    title: "Fitness-Zone",
-    description:
-      "JKD Pakistan is proud to launch its comprehensive weight loss and physical fitness program.",
+    title: "Overseas Recruitment",
+    description: "JKD Pakistan is proud to launch its comprehensive weight loss and physical fitness program.",
     fullDescription: "JKD Pakistan is proud to launch its comprehensive weight loss and physical fitness program through Expert trainers and weight loss specialists, tailored specifically for the people of Peshawar and surrounding areas. Our holistic approach combines cutting-edge techniques with expert guidance to empower individuals in achieving their wellness goals." ,
     pIcon:  program8,
   },
   {
-    title: "Green-Tourism",
-    description:
-      "As a core initiative of the JKD PAKISTAN, Green Tourism is a pioneering travel & tours",
+    title: "Travels and Tours",
+    description: "As a core initiative of the JKD PAKISTAN, Green Tourism is a pioneering travel & tours",
     fullDescription:" As a core initiative of the JKD PAKISTAN, Green Tourism is a pioneering travel & tours consultancy and training organization dedicated to crafting extraordinary, eco-friendly, and responsible travel experiences. Our mission is to promote sustainable tourism practices, support local communities, and preserve Pakistan's natural beauty for future generations. Experience the beauty of the world with our eco-friendly tourism solutions.",
     pIcon: program3,
   },
   {
     title: "Parlour",
-    description:
-      "At JKD Beauty Parlour & Skincare Clinic, we offer expert beauty and skincare solutions tailored to",
+    description: "At JKD Beauty Parlour & Skincare Clinic, we offer expert beauty and skincare solutions tailored to",
     fullDescription:"At JKD Beauty Parlour & Skincare Clinic, we offer expert beauty and skincare solutions tailored to your unique needs. Our state-of-the-art facility provides a relaxing atmosphere, cutting-edge technology, customized training programs and personalized services.",
     pIcon: program4 ,
   },  
   {
     title: "Boutique",
-    description:
-      "JKD Pakistan proudly presents JKD Boutique, a haven for fashion enthusiasts, offering an",
+    description: "JKD Pakistan proudly presents JKD Boutique, a haven for fashion enthusiasts, offering an",
     fullDescription:"JKD Pakistan proudly presents JKD Boutique, a haven for fashion enthusiasts, offering an exclusive range of clothing, dress making, and fashion designs that seamlessly merge traditional craftsmanship with modern aesthetics, epitomizing Pakistani elegance, sophistication and cultural heritage.",
     pIcon: program5,
   },
   {
-    title: "Uplift-Events",
-    description:
-      "As a vital part of the JKD PAKISTAN, Uplift-Events is a dynamic and innovative event management",
+    title: "Uplift Events",
+    description: "As a vital part of the JKD PAKISTAN, Uplift-Events is a dynamic and innovative event management",
     fullDescription:"program dedicated to creating unforgettable experiences that inspire, educate, and uplift individuals and communities. Our team of passionate and experienced professionals is committed to delivering exceptional events that exceed expectations and leave a lasting impact.",
     pIcon: program6 ,
   },  {
     title: "FOODIUM",
-    description:
-      "As a cornerstone of JKD PAKISTAN, Foodium Café, not just a café - serving customers for Pakistani",
+    description: "As a cornerstone of JKD PAKISTAN, Foodium Café, not just a café - serving customers for Pakistani",
     fullDescription:"As a cornerstone of JKD PAKISTAN, Foodium Café, not just a café - serving customers for Pakistani traditional foods and fast foods, we're a hub for hospitality training and hotel management. Our café serves as a live classroom, where students can learn and practice the skills needed to succeed in the fast-paced hospitality and hotel industry. Our expert trainers and mentors guide students through hands-on training, workshops, and mentorship programs, focusing on:",
     pIcon: program7 ,
   }
