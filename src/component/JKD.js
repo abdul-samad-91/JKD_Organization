fd7f5a3a393d827755f6ce090e6ea5a502cdc9ba
as03489822 <as03489822@gmail.com>
2025-10-11 10:39:29 +0500

    style jkd component

--- src/component/JKD.js
+++ src/component/JKD.js
@@ -14,7 +14,7 @@ const JKD = () => {
     // const {theme} = useGlobal()
     const theme = "dark";
     const [toggle , setToggle] = useState({
-        vision:false,
+        vision:true,
         mission:false,
         value:false,
     })
@@ -32,12 +32,24 @@ const JKD = () => {
     <section className={`${theme === 'light' ? 'bg-[#eefbff] text-black':'bg-[#eefbff] text-white'}  w-full flex flex-col items-center my-10 py-20`}>
         <div className='w-[1200px] px-4'>
             <div className='flex items-start gap-5'>
-                <div className='flex flex-col gap-5 w-[50%]'>
+                {/* left side */}
+                <div className='flex flex-col gap-2 w-[50%]'>
+                    <h3 className='text-[30px] font-semibold   text-black'>WORLD CLASS <span className={`text-[#177faa]`}>FACILITIES </span></h3>
+                    <p className={` text-start text-black  pt-2 `} >JKD Pakistan is dedicated to excellence in training, education, and development creating skilled professionals who can lead with confidence and make a difference across the globe.</p>
+                    <ul className='text-black list-disc  ml-6 py-4'>
+                        <li>International Standard Training Our programs are designed according to global benchmarks, ensuring every learner gains practical, real-world expertise.</li>
+                        <li>Modern Learning Environment Equipped with advanced tools, labs, and digital resources to support effective hands-on training.</li>
+                        <li>Professional Instructors Learn under the guidance of certified and experienced trainers committed to your success.</li>
+                    </ul>
+                    <p className={` text-start text-black  `} >At JKD Pakistan, we don’t just teach skills we build futures.</p>
+                </div>
+                {/* right side */}
+                <div className='w-[50%] flex flex-col gap-5'>
                     <div className='w-full  p-5 bg-[#00874F]  rounded'>
                         <button onClick={() => handleToggle("mission")} className="cursor-pointer"><FontAwesomeIcon className='text-[20px] transition-transform duration-500' icon={toggle.mission ? faArrowCircleUp : faArrowCircleDown} />
                         <span className='ml-5 font-semibold text-[18px]'>Our Mission</span>
                         </button>
-                          <div 
+                            <div 
                             className={`transition-all duration-500 ease-in-out overflow-hidden ${toggle.mission ? 'max-h-96' : 'max-h-0'}`}
                             aria-hidden={!toggle.vision}
                             >
@@ -58,63 +70,63 @@ const JKD = () => {
                             <p className={`${theme === 'light' ? ' text-gray-700 ':'text-white'}  text-justify pt-4`}>To uplift and empower under-served communities through innovative and sustainable initiatives in vocational training, skill development, sustainable tourism, sports for development, health-care, and organizing events that promote social causes, community cohesion, and cultural exchange</p> 
                         </div>
                     </div>
-                </div>
-                <div className='w-[50%] p-5 bg-[#e98b28] rounded'>
-                        <button onClick={() => handleToggle("value")} className="cursor-pointer"><FontAwesomeIcon className='text-[20px]' icon={toggle.vision ? faArrowCircleUp : faArrowCircleDown} /><span className='ml-5 font-semibold text-[18px]'>Our Values</span>
-                        </button>
-                        <ul className={` transition-all duration-500 ease-in-out overflow-hidden ${toggle.value ? 'max-h-96' : 'max-h-0'}`}
-                            aria-hidden={!toggle.vision}>
-                            <li className="pt-4 flex items-center gap-3 mb-[10px] ">
-                                {/* <FontAwesomeIcon icon={faLightbulb} className="text-yellow-500 text-xl " /> */}
-                                <p>
-                                <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Empowerment:</span> Unlocking potential through skills and resources.
-                                </p>
-                            </li>
+                    <div className='p-5 bg-[#e98b28] rounded'>
+                            <button onClick={() => handleToggle("value")} className="cursor-pointer"><FontAwesomeIcon className='text-[20px]' icon={toggle.vision ? faArrowCircleUp : faArrowCircleDown} /><span className='ml-5 font-semibold text-[18px]'>Our Values</span>
+                            </button>
+                            <ul className={` transition-all duration-500 ease-in-out overflow-hidden ${toggle.value ? 'max-h-96' : 'max-h-0'}`}
+                                aria-hidden={!toggle.vision}>
+                                <li className="pt-4 flex items-center gap-3 mb-[10px] ">
+                                    {/* <FontAwesomeIcon icon={faLightbulb} className="text-yellow-500 text-xl " /> */}
+                                    <p>
+                                    <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Empowerment:</span> Unlocking potential through skills and resources.
+                                    </p>
+                                </li>
 
-                            <li className="flex items-center gap-3 mb-[10px] ">
-                                {/* <FontAwesomeIcon icon={faLeaf} className="text-green-600 text-xl " /> */}
-                                <p>
-                                <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Sustainability:</span> Fostering growth that benefits present and future generations.
-                                </p>
-                            </li>
+                                <li className="flex items-center gap-3 mb-[10px] ">
+                                    {/* <FontAwesomeIcon icon={faLeaf} className="text-green-600 text-xl " /> */}
+                                    <p>
+                                    <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Sustainability:</span> Fostering growth that benefits present and future generations.
+                                    </p>
+                                </li>
 
-                            <li className="flex items-center gap-3 mb-[10px] ">
-                                {/* <FontAwesomeIcon icon={faUsers} className="text-blue-500 text-xl " /> */}
-                                <p>
-                                <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Inclusive:</span> Promoting equality and accessibility for all.
-                                </p>
-                            </li>
+                                <li className="flex items-center gap-3 mb-[10px] ">
+                                    {/* <FontAwesomeIcon icon={faUsers} className="text-blue-500 text-xl " /> */}
+                                    <p>
+                                    <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Inclusive:</span> Promoting equality and accessibility for all.
+                                    </p>
+                                </li>
 
-                            <li className="flex items-center gap-3 mb-[11px] ">
-                                {/* <FontAwesomeIcon icon={faBalanceScale} className="text-gray-600 text-xl " /> */}
-                                <p>
-                                <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Integrity:</span> Upholding transparency, accountability, and ethical conduct.
-                                </p>
-                            </li>
+                                <li className="flex items-center gap-3 mb-[11px] ">
+                                    {/* <FontAwesomeIcon icon={faBalanceScale} className="text-gray-600 text-xl " /> */}
+                                    <p>
+                                    <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Integrity:</span> Upholding transparency, accountability, and ethical conduct.
+                                    </p>
+                                </li>
 
-                            <li className="flex items-center gap-3 mb-[11px] ">
-                                {/* <FontAwesomeIcon icon={faFlask} className="text-purple-500 text-xl " /> */}
-                                <p>
-                                <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Innovation:</span> Embracing creative solutions to social challenges.
-                                </p>
-                            </li>
+                                <li className="flex items-center gap-3 mb-[11px] ">
+                                    {/* <FontAwesomeIcon icon={faFlask} className="text-purple-500 text-xl " /> */}
+                                    <p>
+                                    <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Innovation:</span> Embracing creative solutions to social challenges.
+                                    </p>
+                                </li>
 
-                            <li className="flex items-center gap-3 mb-[11px] ">
-                                {/* <FontAwesomeIcon icon={faHandshake} className="text-orange-500 text-xl " /> */}
-                                <p>
-                                <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Collaboration:</span> Partnering for collective impact.
-                                </p>
-                            </li>
-                            
+                                <li className="flex items-center gap-3 mb-[11px] ">
+                                    {/* <FontAwesomeIcon icon={faHandshake} className="text-orange-500 text-xl " /> */}
+                                    <p>
+                                    <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Collaboration:</span> Partnering for collective impact.
+                                    </p>
+                                </li>
+                                
 
-                            <li className="flex items-center gap-3 mb-[11px] ">
-                                {/* <FontAwesomeIcon icon={faLandmark} className="text-red-500 text-xl " /> */}
-                                <p>
-                                <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Respect for Culture:</span> Honoring and preserving cultural heritage.
-                                </p>
-                            </li>
-                    </ul>
-                        {/* </div> */}
+                                <li className="flex items-center gap-3 mb-[11px] ">
+                                    {/* <FontAwesomeIcon icon={faLandmark} className="text-red-500 text-xl " /> */}
+                                    <p>
+                                    <span className={`font-semibold ${theme === 'light' ? 'text-black' : 'text-white'}`}>Respect for Culture:</span> Honoring and preserving cultural heritage.
+                                    </p>
+                                </li>
+                        </ul>
+                            {/* </div> */}
+                    </div>
                 </div>
             </div>
 
